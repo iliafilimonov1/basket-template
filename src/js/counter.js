884315e3f7bc9fca69ee778960cf93ff17f92d71
Ilia Filimonov <iliafilimonov1@gmail.com>
2025-12-13 11:59:25 +0300

    Базовый функционал компонента счетчик

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,1 +1,23 @@
-// your code
+const counterInput = document.querySelector('.counter__input') // инпут
+const counterBtnUp = document.querySelector('.counter__btn--up') // кнопка увеличения
+const counterBtnDown = document.querySelector('.counter__btn--down') // кнопка уменьшения
+
+counterBtnUp.addEventListener('click', () => {
+  counterInput.value++
+
+  if (counterInput.value >= '1') {
+    counterBtnDown.disabled = false
+  }
+  if (counterInput.value === '10') {
+    counterBtnUp.setAttribute('disabled', true)
+  }
+})
+
+counterBtnDown.addEventListener('click', () => {
+  if (counterInput.value === '1') {
+    counterBtnDown.disabled = true
+  }
+  counterInput.value--
+})
+
+// надо доделать
