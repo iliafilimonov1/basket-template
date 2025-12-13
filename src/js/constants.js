76da27366bf7b2d51a543ac49067fa15ef029409
Ilia Filimonov <iliafilimonov1@gmail.com>
2025-12-13 12:36:55 +0300

    Добавит json-server для имитации бэка. Подготовит базовые api (запросы).

---
+++ src/js/constants.js
@@ -0,0 +1,2 @@
+export const PRODUCTS_LIST = document.querySelector('.products-list') // контейнер для отрисовки товаров
+export const FORM = document.querySelector('#productForm') // форма для создания товара
